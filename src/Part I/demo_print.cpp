b434f504356ca143b4efe257b006a9ed4919d81d
nenjotsu <renenjo.valente@gmail.com>
2024-07-31 20:57:52 +0800

    add explanation to demo print

--- src/Part I/demo_print.cpp
+++ src/Part I/demo_print.cpp
@@ -6,7 +6,10 @@
 #include <iostream>
 
 template<typename T> void linePrinter(const T& x)  {
+    // `if constexpr` is a compile-time conditional statement. It allows the compiler 
+    // to evaluate the condition during compilation and optimize the code accordingly.
     if constexpr (std::is_integral_v<T>)
+        // If `T` is an integral type (like int), this block will be compiled, printing "num: " followed by the value of `x`.
         std::cout << "num: " << x << '\n';
     else if constexpr (std::is_floating_point_v<T>) {
         const auto frac = x - static_cast<long>(x);
